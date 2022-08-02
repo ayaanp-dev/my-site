c15bc74e4b63add897e0f81ec82244302311269b
ayaan panda <ayaan.panda1@gmail.com>
2022-08-02 00:50:51 -0700

    fix contacts

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -6,14 +6,14 @@ function contact() {
       <h1 className="text-white text-4xl ml-20 mt-10 mb-10  pr-text ">🐱‍💻 Contact</h1>
       <div className="flex flex-row  align-center justify-center projects  ">
         {/* <button class="btn btn-outline btn-info ml-4 ">Email</button> */}
-        <label for="my-modal" class="btn btn-outline btn-info mr-4 ml-4 ">
+        <a href="mailto: ayaan.panda1@gmail.com"><label for="my-modal" class="btn btn-outline btn-info mr-4 ml-4 ">
           Email
-        </label>
-        <button class="btn btn-outline btn-success mr-4 ml-4 contacte">Github</button>
-        <button class="btn btn-outline btn-warning ml-4 mr-4 contacte">Twitter</button>
-        <button class="btn btn-outline btn-error ml-4 mr-4 contacte">dev.to </button>
+        </label></a>
+        <a href="https://github.com/ayaanp-dev/"><button class="btn btn-outline btn-success mr-4 ml-4 contacte">Github</button></a>
+        <a href="https://twitter.com/Ayaan_Panda"><button class="btn btn-outline btn-warning ml-4 mr-4 contacte">Twitter</button></a>
+        <a href="https://dev.to/ayaanp"><button class="btn btn-outline btn-error ml-4 mr-4 contacte">dev.to </button></a>
         <p onClick={()=>{
-          //redirect user to ricroll
+          // used to redirect user to ricroll
           window.location = "https://discord.com/users/953722111631970334";
         }} className="btn btn-outline btn-success ml-4 mr-4 contacte">Discord</p>
       </div>
